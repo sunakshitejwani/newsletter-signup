0cdac79e360d1e371cdd48ff16a0033da9a2cdfb
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-27 13:42:45 +0530

    set up signup form, send post request

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get("/",function(){
-    console.log("get request")
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/signup.html");
 })
+
+app.post("/",function(req,res){
+    const firstname = req.body.fname;
+    const lastname = req.body.lname;
+    const email = req.body.email;
+    console.log(firstname, lastname, email);
+});
+
 app.listen(3002,function(){
     console.log("Server is running on port 3002");
 })
