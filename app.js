2294ea4a8cc25f7864ffee25da4277568cc0191d
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-27 18:12:56 +0530

    post data to maichimp server via their API

--- app.js
+++ app.js
@@ -1,23 +1,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res){
-    res.sendFile(__dirname + "/signup.html");
-})
-
-app.post("/",function(req,res){
-    const firstname = req.body.fname;
-    const lastname = req.body.lname;
-    const email = req.body.email;
-    console.log(firstname, lastname, email);
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3002,function(){
-    console.log("Server is running on port 3002");
-})
+app.post("/", function(req, res) {
+  const firstname = req.body.fname;
+  const lastname = req.body.lname;
+  const email = req.body.email;
+  console.log(firstname, lastname, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us6.api.mailchimp.com/3.0/lists/ae2bba6865";
+
+  const options = {
+    method: "POST",
+    auth: "stardust:0facccc598af4ea8d3b6b8aa571287b7-us6"
+  };
+
+  const request2 = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request2.write(jsonData);
+  request2.end();
+});
+
+app.listen(3002, function() {
+  console.log("Server is running on port 3002");
+});
+
+// API key
+// 0facccc598af4ea8d3b6b8aa571287b7-us6
+
+//list-id
+// ae2bba6865
