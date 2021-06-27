73a8c14d080e46d569592c4be0f19d4cfdeac83a
sunakshitejwani <sunakshitejwani@gmail.com>
2021-06-27 13:06:34 +0530

    modify form and style

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const request = require("request");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(){
+    console.log("get request")
+})
 app.listen(3002,function(){
     console.log("Server is running on port 3002");
 })
